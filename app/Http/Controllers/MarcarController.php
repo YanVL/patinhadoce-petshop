bbf0bee0eb28ece96c03e9247357949be15b18d7
YanVl <yanvitor.gti@gmail.com>
2023-01-23 22:27:28 -0300

    Criando o esqueleto e primeiras rotas

---
+++ app/Http/Controllers/MarcarController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MarcarController extends Controller
+{
+    public function marcar() {
+        echo 'Marcar Serviço';
+    }
+}
