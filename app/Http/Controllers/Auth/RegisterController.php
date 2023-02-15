471839d65e3ca51a81f166f8341f6bcce34950cd
YanVl <yanvitor.gti@gmail.com>
2023-02-15 11:26:09 -0300

    api finalizada

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,8 +9,6 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
-
-
 class RegisterController extends Controller
 {
     /*
@@ -66,16 +64,10 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        $user = User::create([
+        return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
-
-        return response()->json([
-            'status' => true,
-            'message' => 'User Created Successfully',
-            'token' => $user->createToken('meuToken')->plainTextToken
-        ], 200);
     }
 }
